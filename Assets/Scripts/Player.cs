8da17a3836cc3b282ce9e1d1f471b719709dab5d
bitowl <git@bitowl.de>
2022-01-21 23:21:26 +0100

    Cannot control in air?

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,6 +9,7 @@ public class Player : MonoBehaviour
     public float speed = 5;
     public float jumpForce = 3;
     public int groundLayer = 6;
+    public bool canControlInAir = false;
 
     private Rigidbody rb;
     private bool doJump;
@@ -28,12 +29,15 @@ public class Player : MonoBehaviour
 
     void FixedUpdate()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
+        if (canControlInAir || onGround)
+        {
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            float moveVertical = Input.GetAxis("Vertical");
 
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+            Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
-        rb.AddForce(movement * speed);
+            rb.AddForce(movement * speed);
+        }
 
         if (doJump && onGround)
         {
